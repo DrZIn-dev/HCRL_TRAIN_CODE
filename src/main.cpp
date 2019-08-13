6f4f3e859711ebbdf70e6813e4416b248ab7b794
DrZin-dev <pasawee.dev@gmail.com>
2019-08-13 21:11:22 +0700

    commit finish

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,34 @@
 #include <PubSubClient.h> //? Client MQTT
 
 //* Prototype Function
+void callback(char *topic, byte *payload, unsigned int length);
+void mqttReconnect();
+void mqttSetup();
 void wifiSetup();
+void mqttTestPub();
 
-//* Params
+//* WIFI STA Details
 #define WiFi_STA_USERNAME "DrZin"       //? WiFi UserName
 #define WiFi_STA_PASSWORD "0985626152a" //? WiFi Password
-#define Intervals 100                   //? Global Interval Time
 
-unsigned long long previousMillis_WiFi = 0;
+//* MQTT Server Details
+#define MQTT_SERVER "soldier.cloudmqtt.com" //?Server URLS
+#define MQTT_PORT 17638                     //? Server Port
+#define MQTT_USERNAME "DrZin"               //? MQTT UserName
+#define MQTT_PASSWORD "61010707"            //? MQTT Password
+#define MQTT_NAME "ESP32_1"                 //?Topic Name
+
+//* Params
+#define Intervals 500 //? Global Interval Time
+unsigned long long previousMillis_WiFi = 0,
+                   previousMillis_MQTT = 0,
+                   previousMillis_PUB = 0;
+
+//* SUB Topic
+#define MQTT_SUB_TEST "/ESP32_1/Test" //? Test MQTT Only
+
+WiFiClient client;         //? Create TCP Connection
+PubSubClient mqtt(client); //? Create Mqtt over WiFiClient
 
 void setup()
 {
@@ -18,11 +38,21 @@ void setup()
   Serial.begin(115200);
   Serial.println();
   wifiSetup();
+  mqttSetup();
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
+  if (mqtt.connected() == false)
+  {
+    mqttReconnect();
+  }
+  else
+  {
+    mqtt.loop();
+  }
+  mqttTestPub();
 }
 
 void wifiSetup()
@@ -52,4 +82,58 @@ void wifiSetup()
   Serial.println("--- WiFi Connected ---");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
+}
+
+void mqttSetup()
+{
+  mqtt.setServer(MQTT_SERVER, MQTT_PORT); //? Set Destination Server
+  mqtt.setCallback(callback);             //? Set Callback Function
+  Serial.println("--- MQTT Connected ---");
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  payload[length] = '\0';
+  String topic_str = topic, payload_str = (char *)payload;
+  Serial.println("[" + topic_str + "]: " + payload_str);
+}
+void mqttReconnect()
+{
+  Serial.println("MQTT Connecting ");
+  Serial.print("Status: [");
+  while (mqtt.connected() == false)
+  {
+    mqtt.connect(MQTT_NAME, MQTT_USERNAME, MQTT_PASSWORD); //? Reconnect To Server
+
+    //* Time Loop
+    unsigned long currentMillis = millis(); //? Store Current time
+    if (currentMillis - previousMillis_MQTT > Intervals)
+    {
+      previousMillis_MQTT = currentMillis; //? Save Current time
+      Serial.print("=");
+    }
+  }
+  Serial.println("] 100%");
+  Serial.println("--- MQTT Reconnected ---");
+
+  if (mqtt.connect(MQTT_NAME, MQTT_USERNAME, MQTT_PASSWORD))
+  {
+    Serial.println("connected : " + String(MQTT_SUB_TEST));
+    mqtt.subscribe(MQTT_SUB_TEST);
+  }
+  else
+  {
+    Serial.println("--- failed ---");
+  }
+}
+
+void mqttTestPub()
+{
+  unsigned long currentMillis = millis(); //? Store Current time
+  if (currentMillis - previousMillis_PUB > Intervals)
+  {
+    previousMillis_PUB = currentMillis; //? Save Current time
+
+    mqtt.publish(MQTT_SUB_TEST, "Hello From M5GO");
+    }
 }
